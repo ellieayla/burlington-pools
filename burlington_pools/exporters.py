faa19d7f4731c6b6cbf1b4918c55a70741020c40
ellieayla <1447600+me@users.noreply.github.com>
2026-01-11 01:54:22 -0500

    Stable export order

--- burlington_pools/exporters.py
+++ burlington_pools/exporters.py
@@ -50,4 +50,6 @@ class ICalItemExporter(BaseItemExporter):
 
 
     def finish_exporting(self):
-        self.file.write(self.cal.to_ical())
+        self.cal.add_missing_timezones()
+        self.cal.subcomponents = sorted(self.cal.subcomponents, key=lambda e: e.get("UID"))  # stable ordering; sort top-level subcomponents by UID
+        self.file.write(self.cal.to_ical(sorted=True))  # stable ordering; sort properties
